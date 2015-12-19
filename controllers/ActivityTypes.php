fb24c968a5ed877308de5cd232f0752536611e87
Trias Nur Rahman <contact@triasrahman.com>
2015-12-19 21:16:46 +0700

    Create activity types backend

---
+++ controllers/ActivityTypes.php
@@ -0,0 +1,27 @@
+<?php namespace Vhiearch\Useractivity\Controllers;
+
+use BackendMenu;
+use Backend\Classes\Controller;
+use System\Classes\SettingsManager;
+
+/**
+ * Activity Types Back-end Controller
+ */
+class ActivityTypes extends Controller
+{
+    public $implement = [
+        'Backend.Behaviors.FormController',
+        'Backend.Behaviors.ListController'
+    ];
+
+    public $formConfig = 'config_form.yaml';
+    public $listConfig = 'config_list.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        BackendMenu::setContext('October.System', 'system', 'settings');
+        SettingsManager::setContext('Vhiearch.UserActivity', 'useractivity');
+    }
+}
