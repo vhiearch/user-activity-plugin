0e531e3294c7d7bda38d7e17a4086bb7c90e79a4
Trias Nur Rahman <contact@triasrahman.com>
2015-12-19 22:09:44 +0700

    Model options dropdown on backend

--- models/ActivityType.php
+++ models/ActivityType.php
@@ -1,6 +1,9 @@
 <?php namespace Vhiearch\UserActivity\Models;
 
 use Model;
+use RegexIterator;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 
 /**
  * ActivityType Model
@@ -35,5 +38,41 @@ class ActivityType extends Model
     public $morphMany = [];
     public $attachOne = [];
     public $attachMany = [];
+
+    public function getModelOptions()
+    {
+        $path = plugins_path() ;
+        $fqcns = array();
+
+        $allFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+        $phpFiles = new RegexIterator($allFiles, '/\.php$/');
+
+        foreach ($phpFiles as $phpFile) {
+            $content = file_get_contents($phpFile->getRealPath());
+            $tokens = token_get_all($content);
+            $namespace = '';
+
+            for ($index = 0; isset($tokens[$index]); $index++) {
+                if (!isset($tokens[$index][0])) {
+                    continue;
+                }
+                if (T_NAMESPACE === $tokens[$index][0]) {
+                    $index += 2; // Skip namespace keyword and whitespace
+                    while (isset($tokens[$index]) && is_array($tokens[$index])) {
+                        $namespace .= $tokens[$index++][1];
+                    }
+                }
+
+                if (T_CLASS === $tokens[$index][0]) {
+                    $index += 2; // Skip class keyword and whitespace
+                    $fqcns[] = $namespace.'\\'.$tokens[$index][1];
+                }
+            }
+        }
+
+        //TODO: filter only models
+
+        return $fqcns;
+    }
 
 }
